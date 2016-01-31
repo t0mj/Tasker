3fa993c75c63dfb48632b4775449220ca7d24ec9
Thomas Johns <stratic@gmail.com>
2016-01-30 19:33:03 -0600

    Add move command, fix some formatting

--- tasks.py
+++ tasks.py
@@ -13,9 +13,11 @@ from dateutil.relativedelta import relativedelta
 from subprocess import call
 
 
+# Globals
 DIR = os.path.expanduser('~/.tasker')
 DATA_FILE = os.path.join(DIR, 'task_data.yml')
 TODAY = datetime.date.today()
+# Output colors
 BOX = lambda checked: u"\u2705" if checked else u"\u25FB\uFE0F"
 CYAN = lambda txt: click.echo(click.style(txt, fg='cyan'))
 GREEN = lambda txt: click.echo(click.style(txt, fg='green', bold=True))
@@ -39,7 +41,7 @@ class Tasker(object):
         # Returns a list with the task for a given day
         if type(task_date) != datetime.date:
             task_date = parse(task_date).date()
-        one_days_data = self.task_data.get(task_date)
+        one_days_data = self.task_data.get(task_date, [])
         if not one_days_data:
             RED('No tasks found on {}'.format(task_date))
         return one_days_data
@@ -63,6 +65,7 @@ class Tasker(object):
         with open(DATA_FILE, 'w') as tdf:
             yaml.dump(self.task_data, tdf)
 
+
 pass_tasker = click.make_pass_decorator(Tasker)
 
 
@@ -74,6 +77,7 @@ def cli(ctx):
     Tasker is a CLI to track and manage your daily tasks.
     """
     ctx.obj = Tasker()
+    ctx.obj.load_data()
 
 
 @cli.command()
@@ -86,7 +90,6 @@ def add(tasker, task_title, link):
     Add a new task to todays list.\n
     ex: task add Respond to meanface Sue's email
     """
-    tasker.load_data()
     tasker.task_data.setdefault(TODAY, [])
     task_dict = {'title': ' '.join(task_title), 'complete': False}
     if link:
@@ -100,7 +103,7 @@ def add(tasker, task_title, link):
 @cli.command()
 @click.argument('task_idx', type=int, required=True)
 @click.option('--task_date', '-d', default=TODAY,
-              help='Pass a date to remove, defaults to today.')
+              help='Pass a task date, defaults to today.')
 @pass_tasker
 def browse(tasker, task_idx, task_date):
     """
@@ -109,7 +112,6 @@ def browse(tasker, task_idx, task_date):
     task browse 0 -d 2016-01-28\n
     task browse 3
     """
-    tasker.load_data()
     daily_tasks = tasker.daily_tasks(task_date)
     if daily_tasks:
         if task_idx >= len(daily_tasks):
@@ -123,7 +125,7 @@ def browse(tasker, task_idx, task_date):
 @cli.command()
 @click.argument('task_idx', type=int, required=True)
 @click.option('--task_date', '-d', default=TODAY,
-              help='Pass a date to remove, defaults to today.')
+              help='Pass a task date, defaults to today.')
 @pass_tasker
 def done(tasker, task_idx, task_date):
     """
@@ -132,7 +134,6 @@ def done(tasker, task_idx, task_date):
     task done 2\n
     task done 0 -d 2016-01-30
     """
-    tasker.load_data()
     daily_tasks = tasker.daily_tasks(task_date)
     if daily_tasks:
         if task_idx >= len(daily_tasks):
@@ -147,35 +148,33 @@ def done(tasker, task_idx, task_date):
 @cli.command()
 @click.option('--all_tasks', '-a', is_flag=True,
               help='List all tasks by date.')
+@click.option('--task_date', '-d', default=TODAY,
+              help='Pass a task date, defaults to today.')
 @click.option('--week', '-w', is_flag=True,
               help='List all tasks for the last week.')
 @click.option('--month', '-m', is_flag=True,
               help='List all tasks for the current month.')
 @pass_tasker
-def ls(tasker, all_tasks, week, month):
+def ls(tasker, all_tasks, task_date, week, month):
     """
     List todays tasks.
     CYAN tasks have links and can be opened from cli.
     Ex: task open 3
     """
-    tasker.load_data()
     if all_tasks:
-        task_data = tasker.task_data
+        task_list = tasker.task_data
     elif week:
-        task_data = tasker.query_data('weekly')
+        task_list = tasker.query_data('weekly')
     elif month:
-        task_data = tasker.query_data('monthly')
+        task_list = tasker.query_data('monthly')
     else:
-        task_data = {TODAY: tasker.task_data.get(TODAY)}
-        if not task_data[TODAY]:
-            GREEN('No tasks today.')
-            return
-    for task_date, data in task_data.iteritems():
+        task_list = {task_date: tasker.daily_tasks(task_date)}
+    for task_date, data in task_list.iteritems():
         if not data:
             continue
         click.echo()
         YELLOW('Tasks for {}:'.format(task_date))
-        for idx, task in enumerate(tasker.task_data[task_date]):
+        for idx, task in enumerate(data):
             output_str = ('{}  {}. {}'
                           .format(BOX(task['complete']).encode('utf-8'),
                                   idx, task['title']))
@@ -186,9 +185,62 @@ def ls(tasker, all_tasks, week, month):
 
 
 @cli.command()
+@click.argument('task_args', nargs=-1, required=True)
+@click.option('--task_date', '-d', default=TODAY,
+              help='Pass a task date, defaults to today.')
+@pass_tasker
+def mv(tasker, task_args, task_date):
+    """
+    Move a task for the passed index.\n
+    Pass a list in the order you want tasks to completely reorganize.\n
+    Examples:\n
+    task mv 1 up\n
+    task mv 3 down\n
+    task mv 2, 0, 3, 4, 1
+    """
+    directions = {'up': -1, 'down': 1}
+    if len(task_args) == 1:
+        RED('You must give a direction or an order of how you want the tasks.')
+        return
+    daily_tasks = tasker.daily_tasks(task_date)
+    if daily_tasks:
+        dt_copy = list(daily_tasks)
+        if task_args[1] in directions.keys():
+            # Find index of where we're moving and overwrite with copied values
+            task_idx = int(task_args[0])
+            new_loc_idx = task_idx + directions[task_args[1]]
+            if (task_idx >= len(daily_tasks) or task_idx < 0
+                    or new_loc_idx >= len(daily_tasks) or new_loc_idx < 0):
+                RED('Invalid move operation.')
+                return
+            daily_tasks[task_idx] = dt_copy[new_loc_idx]
+            daily_tasks[new_loc_idx] = dt_copy[task_idx]
+            GREEN('Task {} has been moved to task {}'.format(task_idx,
+                                                             new_loc_idx))
+        else:
+            # We're reordering and need to make sure they gave us all values
+            total_idx_args = 0
+            for n in task_args:
+                total_idx_args += int(n)
+            total_idx_dt = 0
+            for task in daily_tasks:
+                total_idx_dt += daily_tasks.index(task)
+            if (total_idx_dt == total_idx_args
+                    and len(daily_tasks) == len(task_args)):
+                # Comparing the total we know each index is represented
+                # Comparing length we know that all positions are there since
+                # index 0 doesn't help in the index totals
+                for idx, val in enumerate(task_args):
+                    daily_tasks[idx] = dt_copy[int(val)]
+                GREEN('Tasks have been reordered.')
+        tasker.sort_dict(tasker.task_data)
+        tasker.write_data()
+
+
+@cli.command()
 @click.argument('task_idx', type=int, required=True)
 @click.option('--task_date', '-d', default=TODAY,
-              help='Pass a date to remove, defaults to today.')
+              help='Pass a task date, defaults to today.')
 @pass_tasker
 def rm(tasker, task_idx, task_date):
     """
@@ -197,7 +249,6 @@ def rm(tasker, task_idx, task_date):
     task rm 0 -d 2016-01-28\n
     task rm 3
     """
-    tasker.load_data()
     daily_tasks = tasker.daily_tasks(task_date)
     if daily_tasks:
         if task_idx >= len(daily_tasks):
